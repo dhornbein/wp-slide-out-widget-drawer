4e81ed7c8bb5c91af1c4b0243e93159f371229b9
Drew Hornbein <dhornbein@gmail.com>
2015-09-11 18:58:05 -0400

    drawer base functionality

--- drawer.php
+++ drawer.php
@@ -29,14 +29,28 @@
     If not, see http://www.gnu.org/licenses/gpl-3.0.html
 */
 
+function drawer_scripts() {
+ 
+    //wp_enqueue_script('jquery');
+ 
+    wp_register_script( 'drawer-script', plugins_url( 'drawer.js', __FILE__ ) , array('jquery'),'',true  );
+    wp_register_style( 'drawer-css', plugins_url( 'drawer.css', __FILE__ ) ,'','', 'screen' );
+ 
+    wp_enqueue_script( 'drawer-script' );
+    wp_enqueue_style( 'drawer-css' );
+ 
+}
+ 
+add_action( 'wp_enqueue_scripts', 'drawer_scripts' );
+
 function drawer_widget() {
 
   register_sidebar( array(
     'name'          => 'Slide Out Sidebar',
     'id'            => 'slide-out-sidebar',
-    'before_widget' => '<div>',
+    'before_widget' => '<div class="slide-out-widget">',
     'after_widget'  => '</div>',
-    'before_title'  => '<h2>',
+    'before_title'  => '<h2 class="slide-out-widget-title">',
     'after_title'   => '</h2>',
   ) );
 
@@ -45,7 +59,7 @@ add_action( 'widgets_init', 'drawer_widget' );
 
 function drawer_html() {
  if ( is_active_sidebar( 'slide-out-sidebar' ) ) : ?>
-    <div id="slide-out-sidebar" class="widget-area" role="complementary">
+    <div id="slide-out-sidebar" class="slide-out-widget-wrapper closed" role="complementary">
       <?php dynamic_sidebar( 'slide-out-sidebar' ); ?>
     </div><!-- #Slide Out Widget -->
   <?php else: ?>
